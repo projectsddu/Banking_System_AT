e7f8136f602b57bc0c8197603eefec90013bfe51
Jenil J Gandhi <63243203+jenilgandhi2111@users.noreply.github.com>
2021-07-14 16:42:29 +0530

    Initial Commit

---
+++ Server/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+const http = require('http');
+const cookieParser = require("cookie-parser")
+const HomeRouter = require("./Routers/HomeRouter")
+
+const HOST = '127.0.0.1';
+const PORT = 80;
+
+
+// Creating server 
+app.listen(PORT, () => {
+    console.log(`Server running at http://${HOST}:${PORT}/`);
+})
+
+// Test path for api
+app.get("/testing", (req, res) => {
+    res.send("Hello world");
+})
+
+app.use(express.json())
+app.use(cookieParser())
+
+//Routers defined here
+app.use(require('./Routers/HomeRouter.js'));
+
+
