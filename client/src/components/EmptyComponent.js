a5540dface01045997739797b1407dc8be3ac32a
Jenil J Gandhi <63243203+jenilgandhi2111@users.noreply.github.com>
2021-07-26 22:44:55 +0530

    Login Page UI created

--- client/src/components/EmptyComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function EmptyComponent() {
-    return (
-        <div className="sideline-1">
-
-        </div>
-    )
-}
--- client/src/components/Login/LoginComponent.js
+++ client/src/components/Login/LoginComponent.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './LoginComponent.css'
-import EmptyComponent from '../EmptyComponent'
 import img from "./Asset/login_asset.jpg"
 
 export default function LoginComponent({ data }) {
     return (
         <>
+            <div>
+                <h2 className="login_header">Bankers</h2>
+            </div>
             <div className="col-6">
                 <div className="row">
                     <img className="Login_Image col-8" src={img} />
@@ -18,21 +20,18 @@ export default function LoginComponent({ data }) {
 
                     <div className="login mb-3">
                         <div className="Login_text">
-                            <h2>Login</h2>
+                            <h2><b>Login</b></h2>
                         </div>
                         <div class="mt-3 mb-2">
-                            <label for="username" class="form-label">User ID / Account Number:</label>
+                            <label for="username" class="form-label"><b>User ID / Account Number:</b></label>
                             <input type="text" class="form-control" id="username" placeholder="Enter the username or account number" />
 
                         </div>
                         <div class="mt-3 mb-3">
-                            <label for="pin_number" class="form-label">Pin Number:</label>
+                            <label for="pin_number" class="form-label"><b>Pin Number:</b></label>
                             <input type="password" class="form-control" id="pin_number" placeholder="Enter PIN number" />
                         </div>
-                        <div class="mb-3">
-                            <u><a className="forgot_link" href="login/password_reset">Forgot Password?</a></u>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Login</button>
+                        <button type="submit" class="btn btn-primary loginbtn">Login</button>
                     </div>
                 </form>
             </div>
